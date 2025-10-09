01a56de18c20895516fb872acdcf5b45d043c683
Victor Bonicenha <141092915+victorbonicenha@users.noreply.github.com>
2025-10-09 10:38:11 -0300

    Divida_Ativa.py

--- Divida_Ativa.py
+++ Divida_Ativa.py
@@ -14,7 +14,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
-from Config_IA import IADividaAtiva
+from config_IA import IADividaAtiva
 from langsmith import traceable
 import os
 
